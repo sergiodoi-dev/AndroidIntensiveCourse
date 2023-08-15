b4101e334956b6e48aca3bcbfcb0f9b2095d8bf1
Sergio Ordonez <sergio.ordonez.co@gmail.com>
2023-08-15 14:53:50 -0500

    Android layouts design

---
+++ Layouts/app/src/main/java/com/cursoant/android/layouts/ui/task/TaskFragment.kt
@@ -0,0 +1,42 @@
+package com.cursoant.android.layouts.ui.task
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.cursoant.android.layouts.databinding.FragmentTaskBinding
+
+class TaskFragment : Fragment() {
+
+    private var _binding: FragmentTaskBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val notificationsViewModel =
+            ViewModelProvider(this).get(TaskViewModel::class.java)
+
+        _binding = FragmentTaskBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textNotifications
+        notificationsViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
