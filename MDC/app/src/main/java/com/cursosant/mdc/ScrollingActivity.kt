1eb95cf8e94a4a131733a013b23cf4d9ffff044b
Sergio Ordonez <sergio.ordonez.co@gmail.com>
2023-08-25 16:16:44 -0500

    MDC App in progress

---
+++ MDC/app/src/main/java/com/cursosant/mdc/ScrollingActivity.kt
@@ -0,0 +1,95 @@
+package com.cursosant.mdc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.webkit.URLUtil
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.cursosant.mdc.databinding.ActivityScrollingBinding
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.snackbar.Snackbar
+
+class ScrollingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityScrollingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityScrollingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fab.setOnClickListener{
+            if (binding.bottomAppBar.fabAlignmentMode == BottomAppBar.FAB_ALIGNMENT_MODE_CENTER){
+                binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
+            }else{
+                binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+           }
+        }
+
+        binding.bottomAppBar.setNavigationOnClickListener{
+            Snackbar.make(binding.root, R.string.message_action_success, Snackbar.LENGTH_LONG)
+                .setAnchorView(binding.fab)
+                .show()
+        }
+
+        binding.content.btnSkip.setOnClickListener { binding.content.cvAd.visibility = View.GONE }
+
+        binding.content.btnBuy.setOnClickListener {
+            Snackbar.make(it, R.string.card_buying, Snackbar.LENGTH_LONG)
+                .setAnchorView(binding.fab)
+                .setAction(R.string.card_to_go) {
+                    Toast.makeText(this, R.string.card_history, Toast.LENGTH_SHORT).show()
+                }
+                .show()
+        }
+
+        loadImage()
+
+        binding.content.cbEnablePass.setOnClickListener {
+            binding.content.tilPassword.isEnabled = !binding.content.tilPassword.isEnabled
+        }
+
+        binding.content.etUrl.onFocusChangeListener = View.OnFocusChangeListener { view, focused ->
+            var errorStr: String? = null
+            val url = binding.content.etUrl.text.toString()
+
+            if(!focused){
+                if (url.isEmpty()){
+                    errorStr = getString(R.string.card_required)
+                }else if(URLUtil.isValidUrl(url)){
+                    loadImage(url)
+                }else{
+                    errorStr = getString(R.string.card_invalid_url)
+                }
+            }
+
+            binding.content.tilUrl.error = errorStr
+        }
+
+    }
+
+    fun loadImage(url: String = "https://wallpaperaccess.com/full/3445875.jpg"){
+        Glide.with(this)
+            .load(url)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .centerCrop()
+            .into(binding.content.imgCover)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_scrolling, menu)
+        return  true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return  when(item.itemId){
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
