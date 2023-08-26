67ec9ca504eea7524c8cdd9b6f46a7f4b82b0eeb
Sergio Ordonez <sergio.ordonez.co@gmail.com>
2023-08-26 14:27:00 -0500

    Material Design Components App

--- MDC/app/src/main/java/com/cursosant/mdc/ScrollingActivity.kt
+++ MDC/app/src/main/java/com/cursosant/mdc/ScrollingActivity.kt
@@ -1,5 +1,6 @@
 package com.cursosant.mdc
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -23,15 +24,15 @@ class ScrollingActivity : AppCompatActivity() {
         binding = ActivityScrollingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.fab.setOnClickListener{
-            if (binding.bottomAppBar.fabAlignmentMode == BottomAppBar.FAB_ALIGNMENT_MODE_CENTER){
+        binding.fab.setOnClickListener {
+            if (binding.bottomAppBar.fabAlignmentMode == BottomAppBar.FAB_ALIGNMENT_MODE_CENTER) {
                 binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
-            }else{
+            } else {
                 binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
-           }
+            }
         }
 
-        binding.bottomAppBar.setNavigationOnClickListener{
+        binding.bottomAppBar.setNavigationOnClickListener {
             Snackbar.make(binding.root, R.string.message_action_success, Snackbar.LENGTH_LONG)
                 .setAnchorView(binding.fab)
                 .show()
@@ -54,16 +55,16 @@ class ScrollingActivity : AppCompatActivity() {
             binding.content.tilPassword.isEnabled = !binding.content.tilPassword.isEnabled
         }
 
-        binding.content.etUrl.onFocusChangeListener = View.OnFocusChangeListener { view, focused ->
+        binding.content.etUrl.onFocusChangeListener = View.OnFocusChangeListener { _, focused ->
             var errorStr: String? = null
             val url = binding.content.etUrl.text.toString()
 
-            if(!focused){
-                if (url.isEmpty()){
+            if (!focused) {
+                if (url.isEmpty()) {
                     errorStr = getString(R.string.card_required)
-                }else if(URLUtil.isValidUrl(url)){
+                } else if (URLUtil.isValidUrl(url)) {
                     loadImage(url)
-                }else{
+                } else {
                     errorStr = getString(R.string.card_invalid_url)
                 }
             }
@@ -71,9 +72,42 @@ class ScrollingActivity : AppCompatActivity() {
             binding.content.tilUrl.error = errorStr
         }
 
+        binding.content.toogleButton.addOnButtonCheckedListener { _, checkedId, _ ->
+            binding.content.root.setBackgroundColor(
+                when (checkedId) {
+                    R.id.btn_red -> Color.RED
+                    R.id.btn_blue -> Color.BLUE
+                    else -> Color.GREEN
+                }
+            )
+        }
+
+        binding.content.swFab.setOnCheckedChangeListener { button, isChecked ->
+            if(isChecked){
+                button.text = getString(R.string.card_hide_fab)
+                binding.fab.show()
+            }else{
+                button.text = getString(R.string.card_show_fab)
+                binding.fab.hide()
+            }
+        }
+
+        binding.content.sldVol.addOnChangeListener { slider, value, fromUser ->
+            binding.content.tvSubtitle.text = "Vol: $value"
+        }
+        
+        binding.content.cpEmail.setOnCheckedChangeListener { chip, isChecked ->
+            if(isChecked){
+                Toast.makeText(this, "${chip.text}", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.content.cpEmail.setOnCloseIconClickListener{
+            binding.content.cpEmail.visibility = View.GONE
+        }
     }
 
-    fun loadImage(url: String = "https://wallpaperaccess.com/full/3445875.jpg"){
+    private fun loadImage(url: String = "https://wallpaperaccess.com/full/3445875.jpg") {
         Glide.with(this)
             .load(url)
             .diskCacheStrategy(DiskCacheStrategy.ALL)
@@ -83,11 +117,11 @@ class ScrollingActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_scrolling, menu)
-        return  true
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return  when(item.itemId){
+        return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
